f5b4e03ae762bbcb9e2b1d4427393f63ce007b28
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-07-16 23:49:33 +0530

    adding profile image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { Navbar } from './components';
 function App() {
   return (
     <div className="app">
-      <Navbar />
+      {/* <Navbar /> */}
       <Header />
       <About />
       <Work />
--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import './Header.scss';
+import React from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import "./Header.scss";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Pabitra</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Competitive Coder</p>
+          </div>
+
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, delayChildren: 0.5 }}
+            className="app__header-img"
+          >
+            <img src={images.profile} alt="profile_background" />
+
+          </motion.div>
+
+          <motion.div></motion.div>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
