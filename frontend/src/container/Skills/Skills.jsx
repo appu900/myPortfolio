491879beaa3baf56aaa4a805557167fdfcffd551
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-08-02 23:16:37 +0530

    adding styles to work experience

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { ReactTooltip } from "react-tooltip";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Skills.scss";
@@ -57,21 +57,16 @@ const Skills = () => {
                       whileInView={{ opacity: [0, 1] }}
                       transition={{ duration: 0.5 }}
                       className="app__skills-exp-work"
-                      data-tip
                       data-for={work.name}
                       key={work.name}
+                      data-tooltip-id="my-tooltip"
+                      data-tooltip-content={work.desc}
                     >
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
                     </motion.div>
-                    {/* <ReactTooltip
-                      id={work.name}
-                      effect="solid"
-                      arrowColor="#fff"
-                      className="skills-tooltip"
-                    >
-                      {work.desc}
-                    </ReactTooltip> */}
+                    <ReactTooltip className="skills-tooltip" id="my-tooltip"
+                    />
                   </>
                 ))}
               </motion.div>
@@ -83,4 +78,4 @@ const Skills = () => {
   );
 };
 
-export default Skills;
+export default AppWrap(Skills, 'skills');
