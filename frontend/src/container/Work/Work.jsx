e37ed054c16f98eb6db75d849d5a1f0e167fa35e
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-08-04 00:05:42 +0530

    adding a HOC for the page animation

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,42 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { AppWrap } from "../../wrapper";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Work.scss";
 
 const Work = () => {
-  const [activeFilter, setActiveFilter] = useState("All");
-  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
   useEffect(() => {
     const query = '*[_type == "works"]';
-    client.fetch(query).then((res) => {
-      setWorks(res);
-      setFilterWork(res);
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
     });
   }, []);
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
-    setAnimateCard({ y: 100, opacity: 0 });
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
     setTimeout(() => {
-      setAnimateCard({ y: 0, opacity: 1 });
-    }, 500);
+      setAnimateCard([{ y: 0, opacity: 1 }]);
 
-    if(item === 'All') {
-      setFilterWork(works);
-    }
-    else {
-      setFilterWork(works.filter(work => work.tags.includes(item)));
-    }
+      if (item === "All") {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
   };
+
   return (
     <>
       <h2 className="head-text">
-        My creative <span>Portfolio </span>Section
+        My Creative <span>Portfolio</span> Section
       </h2>
 
       <div className="app__work-filter">
@@ -112,4 +115,8 @@ const Work = () => {
   );
 };
 
-export default AppWrap(Work, "work");
+export default AppWrap(
+  MotionWrap(Work, "app__works"),
+  "work",
+  "app__primarybg"
+);
